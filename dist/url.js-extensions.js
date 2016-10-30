063588d79b564994bb4afd92274e5f000fa875e2
NielsHolt <niels@steenbuchholt.dk>
2016-10-30 12:39:12 +0100

    Bug fix in demo/index.html

--- dist/url.js-extensions.js
+++ dist/url.js-extensions.js
@@ -147,6 +147,24 @@
     Validate value using validator
     validator = regExp | function( value ) | array of validator
     *******************************************/
+/*
+function tryParseJSON (jsonString){
+    try {
+        var o = JSON.parse(jsonString);
+
+        // Handle non-exception-throwing cases:
+        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+        // but... JSON.parse(null) returns null, and typeof null === "object", 
+        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+        if (o && typeof o === "object") {   //OR $.type( o ) == 'object'
+            return o;
+        }
+    }
+    catch (e) { }
+
+    return false;
+};
+*/
     function validateValue( value, validator ){
         //Convert Boolean into String
         if ($.type( value ) === "boolean")
@@ -223,9 +241,10 @@
                   value = (value == 'true');
                 
                 //Convert String to Float
-                if (options.convertNumber && _this.validateValue( value, '_number') ){
+                if (options.convertNumber && _this.validateValue( value, 'NUMBER') ){
                     value = parseFloat( value );
                 }
+                
                 obj[id] = value;
             });
         }
